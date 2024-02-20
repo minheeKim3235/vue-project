dc8055a19370d90d6ab5b35613a59cd28470cb1d
minheeKim3235 <49574992+minheeKim3235@users.noreply.github.com>
2024-02-20 14:58:58 +0900

    create vue-project

---
+++ src/views/AboutView.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import { IconBook } from '@tabler/icons-vue'
+</script>
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <div>
+      <IconBook :size="64" stroke-width="1"/>
+    </div>
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    flex-flow: column;
+    place-content: center;
+    place-items: center;
+  }
+}
+</style>
